0fa421b84504c7ae97423910288cfaa0021611d8
Till Fischer <wd974864@varian.com>
2025-03-28 16:43:04 +0100

    init project

---
+++ FreedomTaskbar.Core/WinApi.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace FreedomTaskbar.Core
+{
+  public class WinApi
+  {
+    [DllImport("User32.dll")] 
+    public static extern bool EnumDesktopWindows(IntPtr hDesktop, IntPtr lpfn, IntPtr lParam);
+  }
+}
---
+++ FreedomTaskbar/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using FreedomTaskbar.Core;
+
+namespace FreedomTaskbar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var result = WinApi.EnumDesktopWindows(IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
+        }
+    }
+}
